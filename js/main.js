f3ae0049890cebc5d13f0e61755d9c0380807300
Jesús Manuel García Muñoz <hola@jesusmagamu.dev>
2025-12-20 22:04:20 +0100

    Adds hamburger menu for mobile

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-(function() {
+(function () {
   const header = document.querySelector('.header');
   let lastScrollY = window.scrollY;
 
-  window.addEventListener('scroll', function() {
+  // Header Scroll
+  window.addEventListener('scroll', function () {
     const currentScrollY = window.scrollY;
 
     if (currentScrollY > lastScrollY) {
@@ -15,4 +16,23 @@
 
     lastScrollY = currentScrollY;
   });
+
+  // Hamburger Menu
+  const hamburger = document.getElementById('hamburger-menu');
+  const nav = document.getElementById('header-nav');
+
+  if (hamburger && nav) {
+    hamburger.addEventListener('click', function () {
+      hamburger.classList.toggle('is-active');
+      nav.classList.toggle('is-open');
+    });
+
+    // Close menu when clicking a link
+    nav.querySelectorAll('a').forEach(link => {
+      link.addEventListener('click', () => {
+        hamburger.classList.remove('is-active');
+        nav.classList.remove('is-open');
+      });
+    });
+  }
 })();
